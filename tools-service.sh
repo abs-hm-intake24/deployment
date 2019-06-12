99b84dd48222600e51ab10e930b31374d462ec78
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2019-06-12 13:08:31 +0100

    Tools service role

--- tools-service.sh
+++ tools-service.sh
@@ -3,5 +3,5 @@
 if [ "$#" -ne 1 ]; then
   echo "Instance name required"
 else
-  ansible-playbook -i ./instances/$1/hosts -e @./instances/$1/tools-service/config.json --extra-vars="intake24_instance_name=$1" ansible/data-export-service.yml
+  ansible-playbook -i ./instances/$1/hosts -e @./instances/$1/tools-service/config.json --extra-vars="intake24_instance_name=$1" ansible/tools-service.yml
 fi
