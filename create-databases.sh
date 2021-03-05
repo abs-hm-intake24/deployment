bfefc927c088e35c7244282c76849f05be62ac69
piephai <tonphai2522@gmail.com>
2021-03-05 13:14:56 +1000

    Separated common tasks from package manager specific tasks

--- create-databases.sh
+++ create-databases.sh
@@ -3,6 +3,6 @@
 if [ "$#" -ne 1 ]; then
   echo "Instance name required"
 else
-  ansible-playbook -i ./instances/$1/hosts -e @./instances/$1/database/postgres-configuration.yml --extra-vars="intake24_instance_name=$1" ansible/create-databases.yml
+  ansible-playbook -vvv -i ./instances/$1/hosts -e @./instances/$1/database/postgres-configuration.yml --extra-vars="intake24_instance_name=$1" ansible/create-databases.yml
 fi
 
